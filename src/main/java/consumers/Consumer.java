4574d86688b0ce1a0cd080fe380d3c6b655bbeb2
KatePrish <prishchepa@sfedu.ru>
2024-04-11 10:04:31 +0300

    add consumer

--- src/main/java/consumers/Consumer.java
+++ src/main/java/consumers/Consumer.java
@@ -10,6 +10,27 @@ import java.util.Properties;
 
 public class Consumer {
     public static void main(String[] args) {
-       
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", "test-group");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
+        List<String> topics = new ArrayList<String>();
+        topics.add("devglan-partitions-topic");
+        kafkaConsumer.subscribe(topics);
+        try{
+            while (true){
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(10);
+                for (ConsumerRecord<String, String> record: records){
+                    System.out.println(String.format("Topic - %s, Partition - %d, Value: %s", record.topic(), record.partition(), record.value()));
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            kafkaConsumer.close();
+        }
     }
 }
