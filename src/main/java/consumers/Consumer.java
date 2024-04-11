837c35f1a9bf52b23612bec153bfc3df0d871cfa
KatePrish <prishchepa@sfedu.ru>
2024-04-11 09:52:44 +0300

    start

---
+++ src/main/java/consumers/Consumer.java
@@ -0,0 +1,15 @@
+package consumers;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+       
+    }
+}
